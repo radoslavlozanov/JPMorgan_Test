2803ab531638ba4d6208b9c14dcdee32ce7a4b27
rado <radoslav.lozanov@gmail.com>
2015-08-15 16:26:40 +0300

    Initial updates

--- src/main/java/com/jpmorgan/test/bl/StockTradesManager.java
+++ src/main/java/com/jpmorgan/test/bl/StockTradesManager.java
@@ -1,13 +1,12 @@
 package com.jpmorgan.test.bl;
 
-/**
- * Class is holding information about all trades that are done for single stock
- */
-
 import com.jpmorgan.test.pojo.Trade;
 
 import java.util.TreeSet;
 
+/**
+ * Class is holding information about all trades that are done for single stock
+ */
 public class StockTradesManager {
     /** Time frame that is used to calculate Stock price */
     private static final long CALC_TIME = 15 * 60 * 1000;
